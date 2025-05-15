d93a505990939ce7006af79d4336a5a21ba69bc9
Michal Ciesielski <michal.m.ciesielski@voiapp.io>
2025-05-15 16:00:42 +0200

    Add more asserts to test

--- test_main.py
+++ test_main.py
@@ -12,6 +12,8 @@ def test_create_comment():
     path = ft.create_new_comment("Leon", "leon@gmail.com", "Do you dream, Elliot?", comment_fname, ["example",])
 
     comments = ft.get_comments_for_slug("example", [".",])
+
+    assert comments[1].created_by == "Leon"
     assert len(comments) == 2
 
     # Cleanup after yourself!
