0323bcc4496c90d7c779a350a1c5229516ef449f
Michal Ciesielski <ciesielskimm@gmail.com>
2025-06-15 11:13:52 +0200

    Use fixture to cleanup test generated files

--- test_main.py
+++ test_main.py
@@ -1,24 +1,40 @@
 import frontend as ft
 import ulid
 from pathlib import Path
+import pytest
+
+
+@pytest.fixture()
+def file_cleanup(request):
+    """
+    Gives me the ability to defer the file cleanup, in the test body, by
+    appending the paths to this list.
+    """
+    paths_to_cleanup = []
+
+    def cleanup():
+        for p in paths_to_cleanup:
+            p.unlink()
+
+    request.addfinalizer(cleanup)
+    return paths_to_cleanup
+
 
 def test_load_comments():
     comments = ft.get_comments_for_slug("example", [".",])
     assert len(comments) == 1
 
-def test_create_comment():
+def test_create_comment(file_cleanup):
     comments = ft.get_comments_for_slug("example", [".",])
 
     comment_fname = str(ulid.new())
     path = ft.create_new_comment("Leon", "leon@gmail.com", "Do you dream, Elliot?", comment_fname, ["example",])
+    file_cleanup.append(path)
 
     comments = ft.get_comments_for_slug("example", [".",])
 
     assert comments[1].created_by == "Leon"
     assert len(comments) == 2
-
-    # Cleanup after yourself!
-    path.unlink()
 
 def test_comment_api_from_path():
     p = Path("comments", "example", "01H7A86Y8EM4DM3FM25EWPHB8W.txt")
@@ -38,7 +54,7 @@ def test_comment_api_into_file_author_fail():
 
     comments = ft.get_comments_for_slug("example", [".",])
 
-def test_comment_api_into_file():
+def test_comment_api_into_file(file_cleanup):
     comments = ft.get_comments_for_slug("example", [".",])
 
     comment_fname = str(ulid.new())
@@ -49,12 +65,9 @@ def test_comment_api_into_file():
     comment.paragraphs = ["How do I know which one's for me?",]
 
     result_path = comment.dump_into_file(["example",], comment_fname)
+    file_cleanup.append(result_path)
 
     comments = ft.get_comments_for_slug("example", [".",])
 
     assert len(comments[-1].paragraphs) == 1
     assert comments[-1].created_by == "Elliot"
-
-    result_path.unlink()
-
-
